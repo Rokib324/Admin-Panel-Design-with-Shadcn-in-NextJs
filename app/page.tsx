a89e2b47ba0a2372b796798d5fe67368106c2697
Rokibul Islam <rokibulislam@Rokibuls-MacBook-Pro.local>
2025-08-16 02:32:07 +0600

    implement piechart

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import { Button } from "@/components/ui/button";
 import AppBarChart from "@/components/AppBarChart";
 import AppAreaChart from "@/components/AppAreaChart";
+import AppPieChart from "@/components/AppPieChart";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-4 2xl:grid-cols-5 gap-4">
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 "><AppBarChart /></div>
-      <div className="bg-primary-foreground p-4 rounded-lg ">Boxes</div>
+      <div className="bg-primary-foreground p-4 rounded-lg "><AppPieChart /></div>
       <div className="bg-primary-foreground p-4 rounded-lg ">Boxes</div>
       <div className="bg-primary-foreground p-4 rounded-lg ">Boxes</div>
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2"><AppAreaChart /></div>
